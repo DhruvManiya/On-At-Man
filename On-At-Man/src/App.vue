5313c3e1bfd40fc61f13ea191788cf636d78fa8d
Dhruv Maniya <81362979+DhruvManiya@users.noreply.github.com>
2022-04-06 12:05:38 +0530

    first commit

---
+++ On-At-Man/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+
+</template>
+
+
+<script>
+
+export default {
+  
+}
+</script>
+
+
+<style>
+
+:root{
+  green-color:#52ff01;
+}
+
+body{
+  margin: 0;
+  padding: 0;
+  font-family: 'Montserrat', sans-serif;
+}
+
+
+</style>
