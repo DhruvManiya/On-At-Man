306b2114eb07f82a6e10229fda9b793542a3d0f6
Dhruv Maniya <dhruvmaniya12@gmail.com>
2022-05-03 13:22:29 +0530

    eror

--- On-At-Man/src/pages/addSub/AddData.vue
+++ On-At-Man/src/pages/addSub/AddData.vue
@@ -1,18 +1,23 @@
 <template>
     <form @submit.prevent="submitFrom">
           <div class="form-container">
+            <p class="error" v-if="subCode">Enter Subject Code</p>
             <div class="form-control">
-              <label for="code">Subject Code</label>
+              <label for="code">Subject Code *</label>
               <input type="number" id="code" v-model="code">
             </div>
+            <p class="error" v-if="subName">Enter Subject Name</p>
             <div class="form-control">
-              <label for="name">Subject Name</label>
+              <label for="name">Subject Name *</label>
               <input type="text" id="name" v-model="name">
             </div>
+            <p class="error" v-if="stuNumFrom">Enter Your Starting Number</p>
+            <p class="error" v-if="stuNumTo">Enter Your Ending Number</p>
+            <p class="error" v-if="numberCompare">Your Ending Number Should be Gretterthan your Starting Number</p>
             <div class="form-control enroll">
-              <label for="numberFrom">enrollment number From</label>
+              <label for="numberFrom">enrollment number From *</label>
               <input type="number" id="numberFrom" v-model="numberFrom">
-              <label for="numberTo">to</label>
+              <label for="numberTo">to *</label>
               <input type="number" id="numberTo" v-model="numberTo">
             </div>
             <div class="submit-button">
@@ -37,11 +42,32 @@ export default{
       code:null,
       name:'',
       numberFrom:null,
-      numberTo:null
+      numberTo:null,
+      subName:false,
+      subCode:false,
+      stuNumFrom:false,
+      stuNumTo:false,
+      numberCompare:false
     }
   },
   methods:{
     submitFrom() {
+      if(this.code === null) { 
+        this.subCode = true
+      }
+      else if (this.name === '') {
+        this.subName = true
+      }
+      else if (this.numberFrom === null) {
+        this.stuNumFrom = true
+      }
+      else if (this.numberTo === null) {
+        this.stuNumTo = true
+      }
+      else if (this.numberFrom >= this.numberTo ) {
+        this.numberCompare = true
+      }
+      else{
             const formData = {
                 code:this.code,
                 name:this.name,
@@ -49,7 +75,7 @@ export default{
                 numberTo:this.numberTo,
             };
             this.$emit('save-data',formData);
-        
+          }
         
     }
     
@@ -94,5 +120,9 @@ label{
   margin-top: 3rem;
 }
 
+.error{
+  margin: 0 0 0.5rem 2.5rem;
+  color: rgb(255, 41, 41);
+}
 
 </style>
