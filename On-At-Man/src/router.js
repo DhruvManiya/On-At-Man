f56298cc3b9b3ae314c62e41a5ed0c51917815a8
Dhruv Maniya <81362979+DhruvManiya@users.noreply.github.com>
2022-05-08 13:12:50 +0530

    Capitalize component name

--- On-At-Man/src/router.js
+++ On-At-Man/src/router.js
@@ -3,9 +3,9 @@ import { createRouter,createWebHistory} from 'vue-router';
 import Attendence from './pages/attendance/AttendenceSheet.vue'
 import SubjectSheet from './pages/attendance/SubAttendSheet.vue'
 import MyProfile from './pages/Profile/MyProfile.vue'
-import addSubject from './pages/addSub/addSubject.vue'
+import AddSubject from './pages/addSub/addSubject.vue'
 
-import notFound from './pages/NotFound.vue'
+import NotFound from './pages/NotFound.vue'
 
 const router = createRouter({
     history:createWebHistory(),
